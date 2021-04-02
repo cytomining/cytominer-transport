77977bda16625edd337af9e7caa149ba571f8bc4
Allen Goodman <allen.goodman@icloud.com>
2021-04-02 14:45:02 -0400

    data and help text

--- src/cytominer_transport/__main__.py
+++ src/cytominer_transport/__main__.py
@@ -11,16 +11,19 @@ import click
 )
 @click.option(
     "--experiment",
+    help="CSV file with the run details needed to reproduce the other output files",
     nargs=1,
     type=click.File("r")
 )
 @click.option(
     "--image",
+    help="CSV file containing data pertaining to whole images",
     nargs=1,
     type=click.File("r")
 )
 @click.option(
     "--object",
+    help="one or more CSV files containing data pertaining to objects or sub-areas within the whole image",
     multiple=True,
     type=click.File("r")
 )
