c80b20ee34b4fd1764515b9becdbb78b823fe799
Allen Goodman <allen.goodman@icloud.com>
2021-04-02 15:44:30 -0400

    overload

--- src/cytominer_transport/_to_parquet.py
+++ src/cytominer_transport/_to_parquet.py
@@ -2,8 +2,35 @@ import os
 import typing
 
 def to_parquet(
-    source: typing.Union[str, os.PathLike],
     destination: typing.Union[str, os.PathLike],
-    **kwargs
-) -> ():
+    source: typing.Union[str, os.PathLike],
+) -> os.PathLike:
+    pass
+
+@typing.overload
+def to_parquet(
+    destination: typing.Union[str, os.PathLike],
+    image: typing.Union[str, os.PathLike],
+    source: typing.Union[str, os.PathLike],
+) -> os.PathLike:
+    pass
+
+@typing.overload
+def to_parquet(
+    destination: typing.Union[str, os.PathLike],
+    image: typing.Union[str, os.PathLike],
+    object: typing.List[typing.Union[str, os.PathLike]],
+    source: typing.Union[str, os.PathLike],
+) -> os.PathLike:
+    pass
+
+
+@typing.overload
+def to_parquet(
+    destination: typing.Union[str, os.PathLike],
+    experiment: typing.Union[str, os.PathLike],
+    image: typing.Union[str, os.PathLike],
+    object: typing.List[typing.Union[str, os.PathLike]],
+    source: typing.Union[str, os.PathLike],
+) -> os.PathLike:
     pass
